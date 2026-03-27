e72b74b55926afe5ab2677937d3e38c9f961b180
tualal <lenox.tuala@wsu.edu>
2026-03-26 17:57:34 -0700

    Update scripts.js

--- dark-flower/scripts.js
+++ dark-flower/scripts.js
@@ -1,1 +1,11 @@
+const btn = document.querySelector("#darkModeBtn");
 
+btn.addEventListener("click", function () {
+    document.body.classList.toggle("darkMode");
+
+    if (document.body.classList.contains("darkMode")) {
+        btn.textContent = "Light Mode";
+    } else {
+        btn.textContent = "Dark Mode";
+    }
+});
